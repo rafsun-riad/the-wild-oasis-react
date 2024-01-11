381cb7019246c1a5784c8bcfe9a9a07caeedd043
rafsun-riad <mruhaquer@gmail.com>
2024-01-11 20:55:53 +0600

    build some components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -19,13 +14,31 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
-        <Button>Check in</Button>
-        <Button>Check Out</Button>
-        <Input type="number" placeholder="Number of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">The Wild Oasis</Heading>
+              <Button>Check in</Button>
+              <Button variation="secondary" size="small">
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
 }
+Row.defaultProps = {
+  type: 'vertical',
+};
 
 export default App;
