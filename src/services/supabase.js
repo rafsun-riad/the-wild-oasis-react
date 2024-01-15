7cbd90447ed9ca5007befdce6fd0a8ac66ce72e8
rafsun-riad <mruhaquer@gmail.com>
2024-01-15 19:30:00 +0600

    connected with supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://gzhmbknagqidrorggpha.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6aG1ia25hZ3FpZHJvcmdncGhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUyNTI1NjMsImV4cCI6MjAyMDgyODU2M30.8IEMPxNhEGOCfE_eyEAal2QTmQzYewLag0YIasEhwIc';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
