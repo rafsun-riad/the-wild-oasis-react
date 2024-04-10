2033e0299f08f6f30aa33e9fe6894ba794efbfd6
rafsun-riad <mruhaquer@gmail.com>
2024-04-11 00:02:51 +0600

    working with compound component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -3,20 +3,22 @@ import Button from '../ui/Button';
 import CreateCabinForm from '../features/cabins/CreateCabinForm';
 import Modal from '../../ui/Modal';
 
-function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-  return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new Cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
-  );
-}
+function AddCabin() {}
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         Add new Cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
